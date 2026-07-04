5879b37f89102f7675c97b9dd5e484c0343551b7
Grithesh10 <garipellyritheshkumar71@gmail.com>
2026-07-04 13:29:32 +0530

    Connect frontend to Hugging Face API

--- ai-product-frontend/src/pages/Home.jsx
+++ ai-product-frontend/src/pages/Home.jsx
@@ -1,116 +1,36 @@
-import { useState } from "react";
-import axios from "axios";
+const uploadImage = async () => {
 
-import UploadBox from "../components/UploadBox";
-import ProductCard from "../components/ProductCard";
-import PredictionTable from "../components/PredictionTable";
-import ActionButtons from "../components/ActionButtons";
-import LoadingSpinner from "../components/LoadingSpinner";
+  if (!image) {
+    alert("Please select an image");
+    return;
+  }
 
-import "../styles/Home.css";
+  const formData = new FormData();
+  formData.append("file", image);
 
-function Home() {
+  setLoading(true);
 
-  const [image, setImage] = useState(null);
-  const [preview, setPreview] = useState(null);
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
+  try {
 
-  const uploadImage = async () => {
-
-    if (!image) {
-      alert("Please select an image");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", image);
-
-    setLoading(true);
-
-    try {
-
-      const response = await axios.post(
-        "http://127.0.0.1:8000/upload",
-        formData,
-        {
-          headers: {
-            "Content-Type": "multipart/form-data"
-          }
+    const response = await axios.post(
+      "https://grithesh1-ai-product-catalog.hf.space/upload",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
         }
-      );
+      }
+    );
 
-      setResult(response.data);
+    setResult(response.data);
 
-    } catch (err) {
+  } catch (err) {
 
-      console.error(err);
-      alert("Upload Failed");
+    console.error(err);
+    alert("Upload Failed");
 
-    }
+  }
 
-    setLoading(false);
+  setLoading(false);
 
-  };
-
-  return (
-
-    <div className="container">
-
-      <h1>
-        🤖 AI Product Catalog Generator
-      </h1>
-
-      <p className="subtitle">
-
-        Upload any product image and AI will generate
-        complete catalog information.
-
-      </p>
-
-      <UploadBox
-
-        setImage={setImage}
-        setPreview={setPreview}
-        setResult={setResult}
-        preview={preview}
-
-      />
-
-      <button
-        className="analyze-btn"
-        onClick={uploadImage}
-      >
-        Analyze Product
-      </button>
-
-      {loading && <LoadingSpinner />}
-
-      {result && (
-
-        <>
-
-          <ProductCard
-            preview={preview}
-            result={result}
-          />
-
-          <PredictionTable
-            predictions={result.top_predictions}
-          />
-
-          <ActionButtons
-            result={result}
-          />
-
-        </>
-
-      )}
-
-    </div>
-
-  );
-
-}
-
-export default Home;
+};
